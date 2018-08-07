737cdb5ad521e823fa0cebd73ad3ba9ad17ba266
Mark Ehler <ehlerm42@gmail.com>
2018-08-07 17:10:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
   else if(string.toUpperCase()) {
   return "YES INDEED!"
   }
-  else(string === "I love you, Grandma.");
+  else(string = "I love you, Grandma.");
   return "I love you, too!"
   
 }
