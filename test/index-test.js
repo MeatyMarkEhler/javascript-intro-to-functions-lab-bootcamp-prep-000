1c45dbcddd50721c5b64050da448d74802269f83
Mark Ehler <ehlerm42@gmail.com>
2018-08-07 16:40:46 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -25,9 +25,7 @@ describe('logShout(string)', function() {
     console.log.restore()
   })
 })
-function logShout(newwords) {
-  console.log(newwords.toUpperCase())
-}
+
 
 describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
